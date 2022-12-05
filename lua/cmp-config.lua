d46e5c266fef88cb65390759b473a0f34b8932a0
Alex Grishkevich <grishkevich.alexis@gmail.com>
2022-12-05 11:17:06 +0300

    Add autocomplete support.
    Add some plugins for autocomplete and
    add cmp-config.lua with autocomplete options.

---
+++ lua/cmp-config.lua
@@ -0,0 +1,102 @@
+-- luasnip setup
+local cmp = require "cmp"
+local luasnip = require "luasnip"
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+
+  sources = cmp.config.sources({
+    --{ name = 'nvim_lsp' },
+    { name = "luasnip" }, -- for luasnip users.
+    { name = "buffer" },
+    { name = "path" },
+  }),
+
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end
+  },
+
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-1),
+    ["<C-f>"] = cmp.mapping.scroll_docs(1),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  })
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,7 @@ local ensure_packer = function()
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    print("Installing packer close and reopen Neovim...")
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -18,18 +19,39 @@ vim.cmd([[
   augroup end
 ]])
 
+-- Have packer use a popup window
+require("packer").init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
+  use { "wbthomason/packer.nvim" } -- packer can manage itself
+  use { "ellisonleao/gruvbox.nvim" } -- colorschemes
 
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
 
-  -- Colorschemes
-  use "ellisonleao/gruvbox.nvim"
+  -- Completion
+  use { "hrsh7th/nvim-cmp" } -- autocompletion plugin
+  use { "hrsh7th/cmp-buffer" } -- buffer completions
+  use { "hrsh7th/cmp-path" } -- source for filesystem paths.
+  use { "hrsh7th/cmp-cmdline" } -- source for vim's cmdline.
+
+  -- use { "hrsh7th/cmp-nvim-lsp" } -- LSP source for nvim-cmp
+
+  -- Snippet
+  use { "saadparwaiz1/cmp_luasnip" } -- snippets source for nvim-cmp
+  use { "L3MON4D3/LuaSnip" }  -- snippet engine
+  use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
+
+
 
 
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
