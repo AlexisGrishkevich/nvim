ea210a0a7544a570de24ce8cf7bea4ac9b96d4d7
Alex Grishkevich <grishkevich.alexis@gmail.com>
2022-12-22 12:58:39 +0300

    Add some settings to the bufferline-config.lua file.

--- lua/bufferline-config.lua
+++ lua/bufferline-config.lua
@@ -2,7 +2,6 @@ require("bufferline").setup {
   options = {
     always_show_bufferline = true,
     mode = "tabs", -- "buffers"
-    themable = true,
     numbers = "none", -- "ordinal", "buffer_id", "both"
     close_command = "bdelete! %d",
     right_mouse_command = "bdelete! %d",
@@ -28,7 +27,7 @@ require("bufferline").setup {
     offsets = {
       {
         filetype = "NvimTree",
-        text = "File Explorer",
+        text = "",
         text_align = "center",
         separator = false,
         padding = 1
@@ -43,9 +42,86 @@ require("bufferline").setup {
     show_tab_indicators = false,
     show_duplicate_prefix = false, -- whether to show duplicate buffer prefix
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = "thin", -- "slant", "thick"
+    separator_style = "thick", -- "slant", "thick"
     enforce_regular_tabs = false,
     hover = { enabled = false },
-    sort_by = "id", -- "insert_after_current", "insert_at_end", "extension", "relative_directory", "directory", "tabs" 
+    sort_by = "tabs", -- "insert_after_current", "insert_at_end", "extension", "relative_directory", "directory", "tabs" 
+  },
+
+  highlights = {
+    fill = {
+      fg = { attribute = "fg", highlight = "#ff0000" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    background = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    buffer_visible = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    close_button = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    close_button_visible = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    tab = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    tab_selected = {
+      fg = { attribute = "fg", highlight = "Normal" },
+      bg = { attribute = "bg", highlight = "Normal" },
+    },
+
+    tab_close = {
+      fg = { attribute = "fg", highlight = "TabLineSel" },
+      bg = { attribute = "bg", highlight = "Normal" },
+    },
+
+    modified = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    modified_selected = {
+      fg = { attribute = "fg", highlight = "Normal" },
+      bg = { attribute = "bg", highlight = "Normal" },
+    },
+
+    modified_visible = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    indicator_selected = {
+      fg = { attribute = "fg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "Normal" },
+    },
+
+    separator = {
+      fg = { attribute = "bg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    separator_selected = {
+      fg = { attribute = "bg", highlight = "Normal" },
+      bg = { attribute = "bg", highlight = "Normal" },
+    },
+
+    separator_visible = {
+      fg = { attribute = "bg", highlight = "TabLine" },
+      bg = { attribute = "bg", highlight = "TabLine" },
+    }
   }
 }
